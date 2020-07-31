7cdd67a68719587f3aea9494c1820dced6fd9d74
nmudd037 <nithishreddymuddasani.7@gmail.com>
2020-07-31 05:57:50 -0400

    Updates

---
+++ precache-manifest.a53c91e49d967ac2661f9d5f0bca9ebe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe451ce26c9efc7d44f3a921b214485",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/index.html"
+  },
+  {
+    "revision": "53abf4ec7f2ed2f4fe41",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "481f8aa4438c221e17db",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/static/css/main.cc118b9e.chunk.css"
+  },
+  {
+    "revision": "53abf4ec7f2ed2f4fe41",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/static/js/2.047ef2a8.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/static/js/2.047ef2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481f8aa4438c221e17db",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/static/js/main.2a2a828d.chunk.js"
+  },
+  {
+    "revision": "a62548eb576699078a00",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/static/js/runtime-main.df764150.js"
+  },
+  {
+    "revision": "21b2a7f57c3b8ce1e5759820f916814c",
+    "url": "/smartbrain_frontend_facerecognitionwebapp/static/media/Logo2.21b2a7f5.png"
+  }
+]);
